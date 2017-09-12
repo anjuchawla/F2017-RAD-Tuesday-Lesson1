a8c9091e2106558709bf9cbdf0b88c33706136fa
anjuchawla <anju.chawla@georgiancollege.ca>
2017-09-12 16:52:11 -0400

    Created the UI

--- Lesson1Tuesday/frmHelloWorld.Designer.cs
+++ Lesson1Tuesday/frmHelloWorld.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnEnglish = new System.Windows.Forms.Button();
+            this.btnSpanish = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(365, 49);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(122, 29);
+            this.lblHeading.TabIndex = 5;
+            this.lblHeading.Text = "Welcome";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(733, 562);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(96, 17);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Anju Chawla";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMessage.Location = new System.Drawing.Point(118, 145);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(635, 83);
+            this.lblMessage.TabIndex = 6;
+            // 
+            // btnEnglish
+            // 
+            this.btnEnglish.Location = new System.Drawing.Point(206, 340);
+            this.btnEnglish.Name = "btnEnglish";
+            this.btnEnglish.Size = new System.Drawing.Size(103, 44);
+            this.btnEnglish.TabIndex = 0;
+            this.btnEnglish.Text = "&English";
+            this.btnEnglish.UseVisualStyleBackColor = true;
+            // 
+            // btnSpanish
+            // 
+            this.btnSpanish.Location = new System.Drawing.Point(543, 340);
+            this.btnSpanish.Name = "btnSpanish";
+            this.btnSpanish.Size = new System.Drawing.Size(103, 44);
+            this.btnSpanish.TabIndex = 1;
+            this.btnSpanish.Text = "&Spanish";
+            this.btnSpanish.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(118, 470);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(103, 44);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(384, 470);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(103, 44);
+            this.btnPrint.TabIndex = 3;
+            this.btnPrint.Text = "&Print Form";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(650, 470);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 44);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(831, 598);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSpanish);
+            this.Controls.Add(this.btnEnglish);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblHeading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmHelloWorld";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnEnglish;
+        private System.Windows.Forms.Button btnSpanish;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
